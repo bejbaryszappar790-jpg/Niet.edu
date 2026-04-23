c38684ca53912dc282b59b30f7ffe3970b7a7a7b
Beybaris <bejbaryszappar790@gmail.com>
2026-04-23 15:01:02 +0500

    Делаю короче

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
 from app.api.teacher import router as teacher_router
+from app.database import Base, engine
+from app.models import Teacher, Student, Course, Workshop, Enrollment, Video, Progress
+
 
 app = FastAPI()
 
+Base.metadata.create_all(bind = engine)
+
 app.include_router(teacher_router)
-
-
 
