fd8c5fe975df0a3ad4955703a07a07fe2e2116e5
Beybaris <bejbaryszappar790@gmail.com>
2026-04-25 17:31:35 +0500

    Так я подключил роутер студента в main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from app.api.teacher import router as teacher_router
+from app.api.student import router as student_router
 from app.database import Base, engine
-from app.models import Teacher, Student, Course, Workshop, Enrollment, Video, Progress
 
 
 app = FastAPI()
@@ -9,4 +9,6 @@ app = FastAPI()
 Base.metadata.create_all(bind = engine)
 
 app.include_router(teacher_router)
+
+app.include_router(student_router)
 
